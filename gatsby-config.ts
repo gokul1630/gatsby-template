bbc074b201fe3718ccd0febb82731118035a5059
gokul1630 <gokulgp3167@gmail.com>
2022-12-14 10:16:49 +0530

    added gatsby-source-graphql

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,15 +1,19 @@
-import type { GatsbyConfig } from "gatsby"
+require("dotenv").config({
+    path: `.env.${process.env.NODE_ENV}`,
+})
 
-const config: GatsbyConfig = {
-  siteMetadata: {
-    title: `gatsby-template`,
-    siteUrl: `https://www.yourdomain.tld`,
-  },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
-  graphqlTypegen: true,
-  plugins: [],
+module.exports = {
+    plugins: [
+        {
+            resolve: `gatsby-source-graphql`,
+            options: {
+                typeName: `GitHub`,
+                fieldName: `github`,
+                url: `https://api.github.com/graphql`,
+                headers: {
+                    Authorization: `Bearer ${process.env.GATSBY_GITHUB_KEY}`,
+                },
+            },
+        },
+    ],
 }
-
-export default config
